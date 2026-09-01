cbb9d72f29c2e2b9851569d511f28fa6540d1f26
thomasot <74460555+2mastang1@users.noreply.github.com>
2026-09-01 10:33:14 +0200

    Add files via upload

--- KarakterSkala.java
+++ KarakterSkala.java
@@ -1,32 +1,66 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
 public class KarakterSkala {
     public static void main(String args[]){
         int poengsum = 89;
 
+        String[] Elever = {
+            "Mari",
+            "Erik",
+            "Kristin",
+            "Thomas",
+            "Sergiusz"
+        };
+
+
+        Map<String, Integer> poengsummer = new HashMap<>();
+        Random random = new Random();
+
+        for (String person : Elever) {
+            int poeng = random.nextInt(101); // 0–100
+            poengsummer.put(person, poeng);
+        }
+
+        for (String person : poengsummer.keySet()) {
+            System.out.println(
+                person + " har " + poengsummer.get(person) + " poeng"
+            );
+
         String[] karakterer = {
             "A",
+            "A-",
+            "B+",
             "B",
+            "B-",
+            "C+",
             "C",
+            "C-",
+            "D+",
             "D",
             "E",
-            "F",
+            "F"
         };
 
         int[] poeng = {
-            100,90,
-            89,80,
-            79,60,
-            59,50,
-            49,40,
-            39,0,};
+            97,90,
+            87,84,
+            80,76,
+            70,65,
+            62,55,
+            40,0,
+        };
 
-        for (int i=0; i<poeng.length; i+=2){
-            int ovreGrense = poeng[i];
-            int nedreGrense = poeng[i+1];
+        for (int i=0; i<poeng.length; i++){
+            poengsum = poengsummer.get(person);
+            int nedreGrense = poeng[i];
 
-            if (poengsum >= nedreGrense && poengsum <= ovreGrense ){
-                System.out.println(karakterer[i/2]);
+            if (poengsum >= nedreGrense){
+                System.out.println(karakterer[i]);
                 break;
             }
         }
+    }
 }
 }
